0abd05a2425faf02452567f879ab89fadb65b30b
Doncsenko Roman <43998554+DoncsenkoRoman@users.noreply.github.com>
2021-04-24 16:01:00 +0200

    tests

--- test/unit_test.cpp
+++ test/unit_test.cpp
@@ -10,22 +10,56 @@
 #include <gtest/gtest.h>
 
 
-TEST(parserTest, test_filename){
-	std::string filename;
-	std::map<std::string, std::string> testOutput; 
-	std::map<std::string, std::string> expectedOutput;
-	expectedOutput={
-		{"name", "AMD creator daily"},
-	    {"price", "1100000"},
-	    {"manufacturer", "iBuyPower"},
-        {"quantity", "3"},
-        {"ramSize", "32"},
-        {"storage", "2500"}};
-	filename="../computer/Unit1.json";                       
-	testOutput = JSON::Parse(filename);
-	for (auto iter : expectedOutput){
-		ASSERT_EQ(testOutput[iter.first],expectedOutput[iter.first]);
-	}
+TEST(JsonparserTest, JSON_ifstream_work)
+{
+	std::string inputFilename = "../computers/AMDcreatordaily.json";
+	std::map<std::string, std::string> exp_output;
+	exp_output.insert(std::pair<std::string, std::string>("name", "AMD creator daily"));
+	exp_output.insert(std::pair<std::string, std::string>("price", "1100000"));
+	exp_output.insert(std::pair<std::string, std::string>("manufacturer", "iBuyPower"));
+	exp_output.insert(std::pair<std::string, std::string>("quantity", "3"));
+	exp_output.insert(std::pair<std::string, std::string>("ramSize", "32"));
+	exp_output.insert(std::pair<std::string, std::string>("storage", "2500"));
+
+    std::ifstream inputFile(inputFilename);
+
+	std::map<std::string, std::string> test_output = JSON::Parse(inputFile);
+
+    inputFile.close();
+
+	ASSERT_EQ(exp_output, test_output);
+}
+
+TEST(JsonparserTest, JSON_Parse_good_fname)
+{
+	std::string inputFilename = "../computers/AMDcreatordaily.json";
+	std::map<std::string, std::string> exp_output;
+	exp_output.insert(std::pair<std::string, std::string>("name", "AMD creator daily"));
+	exp_output.insert(std::pair<std::string, std::string>("price", "1100000"));
+	exp_output.insert(std::pair<std::string, std::string>("manufacturer", "iBuyPower"));
+	exp_output.insert(std::pair<std::string, std::string>("quantity", "3"));
+	exp_output.insert(std::pair<std::string, std::string>("ramSize", "32"));
+	exp_output.insert(std::pair<std::string, std::string>("storage", "2500"));
+	
+	std::map<std::string, std::string> test_output = JSON::Parse(inputFilename);
+	
+	ASSERT_EQ(exp_output, test_output);
+}
+
+TEST(JsonparserTest, JSON_Parse_bad_fname)
+{
+	std::string inputFilename = "../computers/AMDcreatordaily.json";
+	
+	ASSERT_THROW(JSON::Parse(inputFilename), std::runtime_error);
+}
+
+
+TEST(JsonparserTest, unit_test)
+{
+	
+	std::map<std::string, std::string> maple = JSON::Parse("../computers/AMDcreatordaily.json");
+	std::map<std::string, std::string> sally = JSON::Parse("../computers/Predator.json");
+	ASSERT_FALSE(maple == sally);
 }
 
 
